c24254bac119f808a4a29acf0c1eca7fbe541874
Guillaume RAYNAUD <neodu63@hotmail.fr>
2023-02-01 11:37:51 +0100

    Add full viewset with serializer and make it available

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
 from profiles_api import serializers
 
@@ -43,3 +44,46 @@ class HelloApiView(APIView):
     def delete(self, request):
         """Applies delete method on view"""
         return Response({'message': 'Applies DELETE method'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        """Return a Hello message"""
+
+        a_viewset = [
+            'Uses actions (list, create, retieve, update, partial_update)',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code',
+        ]
+
+        return Response({'message': "Hello!", 'a_viewset': a_viewset})
+
+    def create(self, request):
+        """Creates a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """Performs retrieve"""
+        return Response({'message': 'Performs retrieve like GET'})
+    
+    def update(self, request, pk=None):
+        """Performs update"""
+        return Response({'message': 'Performs update like PUT'})
+    
+    def partial_update(self, request, pk=None):
+        """Performs partial update"""
+        return Response({'message': 'Performs partial update like PATCH'})
+    
+    def destroy(self, request, pk=None):
+        """Performs destroy"""
+        return Response({'message': 'Performs destroy like DELETE'})
